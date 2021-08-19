0a4586bd9ab15c3904b35ec4c48522e2ecf1786c
Akim Rabinko <akim.rabinko@easypark.net>
2021-08-19 09:58:25 +0300

    Initial commit

---
+++ src/main/java/com/example/ec2startstopservice/Ec2StartStopServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ec2startstopservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2StartStopServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ec2StartStopServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ec2startstopservice/Ec2StartStopServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec2startstopservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2StartStopServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
